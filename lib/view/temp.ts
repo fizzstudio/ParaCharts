b4c5e49e67ba7900e955bd42a1a3bd73cc59488b
simonvarey <simonvarey@gmail.com>
2025-03-25 15:22:20 -0400

    Started Map2D

--- lib/view/temp.ts
+++ lib/view/temp.ts
@@ -13,4 +13,41 @@ format(box: Box<Scalar>, context: FormatContext) {
   }
 }
 
+  formatX(context: FormatContext) {
+    return this.model.format(this.x, context);
+  }
+
+  formatY(context: FormatContext) {
+    return this.model.format(this.y, context);
+  }
+
+  format(context: FormatContext) {
+    return `${this.formatX(context)}, ${this.formatY(context)}`;
+  }
+
 */
+
+/** 
+ * Context where a particular value appears. 
+ * @public
+ */
+export type FormatContext = keyof typeof formatContextSettings;
+// Settings that control the format for each context
+const formatContextSettings = {
+  xTick: 'axis.x.tick.labelFormat',
+  yTick: 'axis.y.tick.labelFormat',
+  linePoint: 'type.line.pointLabelFormat',
+  scatterPoint: 'type.scatter.pointLabelFormat',
+  barCluster: 'type.bar.clusterLabelFormat',
+  pieChunk: 'type.pie.chunkLabelFormat',
+  donutChunk: 'type.donut.chunkLabelFormat',
+  gaugeChunk: 'type.gauge.chunkLabelFormat',
+  steplinePoint: 'type.stepline.pointLabelFormat',
+  lollipopPoint: 'type.lollipop.pointLabelFormat',
+  lollipopCluster: 'type.lollipop.clusterLabelFormat',
+  jimX: 'jim.xValueFormat',
+  dataTableX: 'dataTable.xValueFormat',
+  dataTableY: 'dataTable.yValueFormat',
+  statusBar: 'statusBar.valueFormat',
+  domId: 'NA'
+};
