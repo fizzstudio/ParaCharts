b778dc297f68e61cd0f0ef53a5adae5e5105f6d6
simonvarey <simonvarey@gmail.com>
2025-03-25 15:27:07 -0400

    Finished Map2D

--- lib/store/helpers.ts
+++ lib/store/helpers.ts
@@ -4,48 +4,40 @@ type Key = string | number;
 
 type DataRecord<X extends Key, Y extends Key> = { x: X, y: Y };
 
-function mapRecordsInner<X extends Key, Y extends Key, A extends X | Y, B extends X | Y>(
+function mapRecords<X extends Key, Y extends Key>(
   dimension: 'x' | 'y', records: DataRecord<X, Y>[]
-): Record<A, B[]> {
-  const recordsAB = records as DataRecord<A, B>[];
-  const dimensionConst = dimension === 'x' ? 'x' as const : 'y' as const;
-  const map = {} as Record<A, B[]>;
-  for (const record of recordsAB) {
-    if (!(record[dimensionConst] in map)) {
-      map[record[dimensionConst]] = [] as B[];
-    }
-    map
-  }
-}
-
-function mapRecords<X extends string | number, Y extends string | number>(
-  dimension: 'x', records: DataRecord<X, Y>[],
-): Record<X, Y[]>
-function mapRecords<X extends string | number, Y extends string | number>(
-  dimension: 'y', records: DataRecord<X, Y>[],
-): Record<Y, X[]>
-function mapRecords<X extends string | number, Y extends string | number>(
-  dimension: 'x' | 'y', records: DataRecord<X, Y>[]
-): Record<X, Y[]> | Record<Y, X[]> {
-  /*const map = dimension === 'x' ? {} as Record<X, Y[]> : {} as Record<Y, X[]>;
+): Record<(X | Y), (X | Y)[]> {
+  const map = {} as Record<(X | Y), (X | Y)[]>;
   for (const record of records) {
     if (!(record[dimension] in map)) {
-      map[record[dimension]] = [] as B[];
+      map[record[dimension]] = [] as (X | Y)[];
     }
-    map*/
+    map[record[dimension]].push(record[dimension === 'x' ? 'y' : 'x']);
+  }
+  return map;
 }
 
-class Map2D<X extends string | number, Y extends string | number> {
+export class Map2D<X extends string | number, Y extends string | number> {
   private xMap?: Record<X, Y[]>;
   private yMap?: Record<Y, X[]>;
 
-  constructor(private records: DataRecord<X, Y>[]) {}
+  constructor(private records: DataRecord<X, Y>[]) { }
 
   atIndex(index: number): DataRecord<X, Y> | null {
     return this.records[index] ?? null;
   }
 
-  atX(x: X): Y[] {
+  atX(x: X): Y[] | null {
+    if (!(this.xMap)) {
+      this.xMap = mapRecords('x', this.records) as Record<X, Y[]>;
+    }
+    return this.xMap[x] ?? null;
+  }
 
+  atY(y: Y): X[] | null {
+    if (!(this.yMap)) {
+      this.yMap = mapRecords('y', this.records) as Record<Y, X[]>;
+    }
+    return this.yMap[y] ?? null;
   }
 }
