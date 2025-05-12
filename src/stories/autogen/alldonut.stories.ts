4871c45b21e1c78f819d0e59a881dcad98940ec6
simonvarey <simonvarey@gmail.com>
2025-05-11 23:30:37 -0500

    Added donut stories

---
+++ src/stories/autogen/alldonut.stories.ts
@@ -0,0 +1,48 @@
+import { Chart, type ChartProps } from '../Chart';
+import { familyManifestPathsMap } from '../chartSelectorHelper';
+import type { Meta, StoryObj } from "@storybook/web-components";
+
+type Story = StoryObj<ChartProps>;
+
+const titleToFilenameMap = familyManifestPathsMap('pastry', false);
+
+const meta = {
+  title: "Chart/Donut Charts",
+  render: (args) => Chart(args),
+  argTypes: {
+    filename: {
+      description: 'Chart Title',
+      control: {type: 'select'},
+      options: Object.keys(titleToFilenameMap),
+      mapping: titleToFilenameMap
+    }
+  },
+} satisfies Meta<ChartProps>;
+
+export default meta;
+
+export const AllDonutCharts: Story = {
+  name: 'All Donut Charts',
+  args: {
+    filename: '',
+    config: { // change to configFile: "./sample_config.json",
+      "ui.colorVisionMode": "deutan"
+    },
+    /*keybindings: { // change to keybindingsFile: './sample_keybindings.json',
+      "chart": {
+        "a": {
+          "action": "move_left"
+        },
+        "d": {
+          "action": "move_right"
+        },
+        "w": {
+          "action": "move_up"
+        },
+        "s": {
+          "action": "move_down"
+        }
+      }
+    }*/
+  }
+};
