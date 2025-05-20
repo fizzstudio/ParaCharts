89de36d1095304b86a523282f9311bd791b9948c
Doug Schepers <doug@schepers.cc>
2025-05-20 13:49:48 -0400

    Small tweaks to SVG root aria-label, may relate to #98

--- lib/paraview/paraview.ts
+++ lib/paraview/paraview.ts
@@ -427,12 +427,12 @@ export class ParaView extends logging(ParaComponent) {
     this.log('render');
     return html`
       <svg
-        ${ref(this._rootRef)}
-        xmlns=${SVGNS}
-        aria-label=${this._documentView ? `${this._documentView.titleText}, Sonified chart` : 'loading...'}
-        data-charttype=${this.type}
         role="application"
         tabindex="0"
+        aria-label=${this._documentView ? `${this._documentView.titleText}, accessible ${this.type} chart` : 'loading...'}
+        ${ref(this._rootRef)}
+        xmlns=${SVGNS}
+        data-charttype=${this.type}
         height=${fixed`${this._viewBox.height}px`}
         class=${classMap(this._rootClasses())}
         viewBox=${fixed`${this._viewBox.x} ${this._viewBox.y} ${this._viewBox.width} ${this._viewBox.height}`}
