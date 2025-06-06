1ce8ac271c8d93cba4c455794d5dd4c074dc3083
Josh Fennell <josh@jfnl.org>
2025-06-05 22:02:24 -0400

    Remove initial cpanel focus.

--- lib/view/data/chart_landing.ts
+++ lib/view/data/chart_landing.ts
@@ -53,8 +53,6 @@ export class ChartLandingView extends View {
   }
 
   onFocus(isNewComponentFocus = false) {
-    // Set browser focus on our SVG group
-    this.parent.dataset.focus();
     this.paraview.store.visit([]);
     this.paraview.store.announce(this.paraview.summarizer.getChartSummary());
   }
