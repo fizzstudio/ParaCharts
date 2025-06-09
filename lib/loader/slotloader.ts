6488d90bea252bc7c5b4e9bd80e71413cf080251
Samuel <77805918+SamReid4321@users.noreply.github.com>
2025-06-09 06:03:03 -0400

    Added direct loading demo page, various changes to slotloader

--- lib/loader/slotloader.ts
+++ lib/loader/slotloader.ts
@@ -71,55 +71,67 @@ export class SlotLoader {
   }
 
   validateManifest(els: HTMLElement[], manifest: Manifest): {result: string, manifest: Manifest | undefined}{
-    //console.log("validateManifest start")
-    //console.log(els)
-    //console.log(manifest)
-    //console.log("test1")
-    //console.log(manifest.datasets[0].title)
-    //console.log("test2")
-    if (!manifest.datasets[0].title){
-      manifest.datasets[0].title = this.findManifestTitle(els, manifest)
+    const table = els[0] as HTMLTableElement;
+    const dataset = manifest.datasets[0]
+    if (!dataset.title){
+      dataset.title = this.findManifestTitle(els, manifest)
     }
-    //console.log("this.loadManifestFromElement(els)")
-    //console.log(this.loadManifestFromElement(els).facets)
-    //console.log("existing facets")
-    //console.log(manifest.datasets[0].facets)
-    if (!manifest.datasets[0].facets){
-      manifest.datasets[0].facets = this.loadManifestFromElement(els).facets
+    const vars = this.loadHeaders(table.rows[0]);
+    const facets = this.loadFacets(table.rows[0])
+
+    if (!dataset.facets){
+      dataset.facets = facets
     }
-    if (manifest.datasets[0]!.facets){
-      if (!manifest.datasets[0]!.facets.keys){
-        manifest.datasets[0].facets = this.loadManifestFromElement(els).facets
+    if (dataset!.facets){
+      if (!dataset!.facets.keys){
+        dataset.facets = facets
       }
     }
-    if (!manifest.datasets[0]!.series){
-      let series = {key: manifest.datasets[0].facets.y.label, theme: {baseQuantity: '', baseKind: "number" as "number"}, records: this.loadDataFromElement(els, manifest)}
-      manifest.datasets[0].series = [series]
+    if (!dataset!.series){
+      let series = {key: dataset.facets.y.label, theme: {baseQuantity: '', baseKind: "number" as "number"}, records: this.loadDataFromElement(els, manifest)}
+      dataset.series = [series]
     }
-    if (manifest.datasets[0]!.series){
-      if (manifest.datasets[0]!.series.keys.length == 0){
-      let series = {key: manifest.datasets[0].facets.y.label, theme: {baseQuantity: '', baseKind: "number" as "number"}, records: this.loadDataFromElement(els, manifest)}
-      manifest.datasets[0].series = [series]
+    if (dataset!.series){
+      if (dataset!.series.keys.length == 0){
+      let series = {key:dataset.facets.y.label, theme: {baseQuantity: '', baseKind: "number" as "number"}, records: this.loadDataFromElement(els, manifest)}
+      dataset.series = [series]
       }
     }
     if (manifest!.datasets[0]!.series[0]!.records!.length == 0){
-      manifest.datasets[0].series[0].records = this.loadDataFromElement(els, manifest)
+      dataset.series[0].records = this.loadDataFromElement(els, manifest)
     }
-    if (manifest!.datasets[0]!.series[0]!.records!.length == 0){
-      manifest.datasets[0].series[0].records = this.loadDataFromElement(els, manifest)
+    if (!dataset!.type){
+      dataset!.type = this.findManifestType(els, manifest) as "line" | "column" | "bar" | "scatter" | "lollipop" | "stepline" | "pie" | "donut";;
     }
-    if (!manifest.datasets[0]!.type){
-      manifest.datasets[0]!.type = this.findManifestType(els, manifest) as "line" | "column" | "bar" | "scatter" | "lollipop" | "stepline" | "pie" | "donut";;
+    if (!dataset!.data){
+      dataset!.data = {source: "inline"}
     }
-    if (!manifest.datasets[0]!.data){
-      manifest.datasets[0]!.data = {source: "inline"}
+    if (!dataset!.settings){
+      dataset!.settings = {"sonification.isEnabled": true}
     }
-    if (!manifest.datasets[0]!.settings){
-      manifest.datasets[0]!.settings = {"sonification.isEnabled": true}
+
+    const cols = vars.map(v => [] as string[]);
+    Array.from(table.rows).slice(1).forEach((tr: HTMLTableRowElement) => {
+      const vals = this.loadRow(tr, vars);
+      vals.forEach((val, j) => {
+        cols[j].push(val);
+      });
+    });
+    for (let i = 0; i < cols.length; i++) {
+      let facet = dataset.facets[Object.keys(dataset.facets)[i]]
+      if (/%/.test(cols[i][0])) {
+        facet.measure = "ratio"
+      }
+      if (/\b(year|month|day)\b/i.test(vars[i].label)) {
+        facet.datatype = "date"
+      }
+      else if (/\d/.test(cols[i][0])) {
+        facet.datatype = "number"
+      }
     }
     return {result: "success", manifest: manifest}
   }
-
+/*
   loadManifestFromElement(els: HTMLElement[]): Dataset{
     //console.log("Start: loadDataFromElement")
     if (!els.length || els[0].tagName !== 'TABLE') {
@@ -142,19 +154,15 @@ export class SlotLoader {
       }
     }
 
-    for (let i = 0; i < cols.length; i++){
-      if (/\d/.test(cols[i][0])) {
-        vars[i].datatype = "number"
-      }
-    }
+    
     //console.log("cols")
     //console.log(cols)
-    const types: Datatypes = {};
-    /*
-    vars.forEach(v => {
-      types[v.name] = v.type;
-    });
-    */
+    //const types: Datatypes = {};
+    
+    //vars.forEach(v => {
+    ///  types[v.name] = v.type;
+    //});
+    
     //const indepVar = vars.find(v => v.variableType == "independent") ?? vars[0];
     const data: Dataset = {
       type: els[0].getAttribute("type") as "line" | "column" | "bar" | "scatter" | "lollipop" | "stepline" | "pie" | "donut" ?? "bar",
@@ -165,13 +173,6 @@ export class SlotLoader {
       series: [{key: "test", theme: {baseQuantity: "baseQuantity", baseKind: "number"}, records: []}],
       data: {source: "inline"},
     };
-   /*
-    vars.forEach((v, i) => {
-      if (v.variableType == "dependent") {
-        data.series[0].records?.push({x: cols[0], y: cols[1]});
-      }
-    });
-*/
     for (let i = 0; i < cols[0].length; i++){
       data.series[0].records!.push({x: cols[0][i], y: cols[1][i]});
     }
@@ -180,7 +181,7 @@ export class SlotLoader {
     data.facets["x"].variableType = "independent"
     return data;
   }
-
+*/
   loadDataFromElement(els: HTMLElement[], manifest: Manifest){
     //console.log("loadDataFromElement start")
     let facetLabels = [manifest.datasets[0].facets.x.label, manifest.datasets[0].facets.y.label]
@@ -202,16 +203,7 @@ export class SlotLoader {
         cols[j].push(val);
       });
     });
-    for (let i = 0; i < cols.length; i++){
-      if (/%/.test(cols[i][0])) {
-        vars[i].measure = "ratio"
-      }
-    }
-    for (let i = 0; i < cols.length; i++){
-      if (/\d/.test(cols[i][0])) {
-        vars[i].datatype = "number"
-      }
-    }
+
     /*
 console.log("kid")
     console.log(kid)
@@ -247,14 +239,15 @@ console.log("kid")
   }
 
   private loadHeaders(el?: HTMLTableRowElement) {
+    const paraChart = document.getElementsByTagName("para-chart")[0]
     //console.log("loadHeaders start")
     if (!el) {
       throw new Error('table must include a header row');
     }
     const facets: Facet[] = [];
     //console.log(el)
-    for (const kid of el.children) {
-      //console.log(kid)
+    let i = 0;
+    for (const kid of el.children) {      
       if (kid.tagName !== 'TD' && kid.tagName !== 'TH') {
         throw new Error("immediate children of 'tr' element must be 'td' or 'th' elements");
       }
@@ -262,7 +255,7 @@ console.log("kid")
       if (!name) {
         throw new Error("var name must be provided as 'td' element text content");
       }
-      const type = (kid as HTMLElement).dataset.type ?? 'string';
+      let type = (kid as HTMLElement).dataset.type ?? 'string';
       //console.log((kid as HTMLElement).dataset)
       //console.log(type)
       if (!['string', 'number', 'date'].includes(type)) {
@@ -270,18 +263,34 @@ console.log("kid")
       }
       const isIndep = (kid as HTMLElement).dataset.independent === 'true' ? "independent" : "dependent";
       let measure = (kid as HTMLElement).dataset.measure ?? 'nominal';
-      const displayType = (kid as HTMLElement).dataset.displayType ?? { type: "marking" }
+
+      const isRadial =paraChart.type == "pie" || paraChart.type == "donut"
+      const horizVert = ["horizontal", "vertical"]
+      let displayType;
+      if (isRadial){
+         displayType = (kid as HTMLElement).dataset.displayType ?? { type: "marking" }
+      }
+      else{
+         displayType = (kid as HTMLElement).dataset.displayType ?? { type: "axis", orientation: horizVert[i] }
+      }
       facets.push({
         label: name,
-        datatype: type as Datatype ?? "string",
+        datatype: type as Datatype,
         variableType: isIndep,
         measure: measure as "nominal" | "ordinal" | "interval" | "ratio",
         displayType: displayType as DisplayType
       })
+      i++
     }
     return facets;
   }
 
+  private loadFacets(el?: HTMLTableRowElement){
+    let vars = this.loadHeaders(el);
+    let facets = {x: vars[0], y: vars[1]};
+    facets["x"].variableType = "independent"
+    return facets;
+  }
   private loadRow(el: HTMLTableRowElement | null, vars: Facet[]) {
     //console.log("Start: loadRow")
     //console.log(el)
@@ -330,10 +339,13 @@ console.log("kid")
     if (document.getElementsByTagName("para-chart")[0].hasAttribute("type")){
       return document.getElementsByTagName("para-chart")[0].getAttribute("type") as string
     }
+    /*
     let title = this.findManifestTitle(els, manifest);
     const table = els[0] as HTMLTableElement;
     const vars = this.loadHeaders(table.rows[0]);
     const radialKeywords = ["proportion", "proportions", "ratio"]
     return "line"
+    */
+    throw new Error("Error: please provide a type for the para-chart")
   }
 }
