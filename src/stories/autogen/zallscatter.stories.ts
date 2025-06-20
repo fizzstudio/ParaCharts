41b63a2f43a095c06f26944e57ab34dc5d04dc66
simonvarey <simonvarey@gmail.com>
2025-06-20 14:01:27 -0400

    Added lowercase 'Z' All Stories

---
+++ src/stories/autogen/zallscatter.stories.ts
@@ -0,0 +1,30 @@
+import { Chart, type ChartProps } from '../Chart';
+import { familyManifestPathsMap } from '../chartSelectorHelper';
+import type { Meta, StoryObj } from "@storybook/web-components";
+
+type Story = StoryObj<ChartProps>;
+
+const titleToFilenameMap = familyManifestPathsMap('scatter', false);
+
+const meta = {
+  title: "Basic Charts/Scatter Charts",
+  render: (args) => Chart(args),
+  argTypes: {
+    filename: {
+      description: 'Chart Title',
+      control: {type: 'select'},
+      options: Object.keys(titleToFilenameMap),
+      mapping: titleToFilenameMap
+    }
+  },
+} satisfies Meta<ChartProps>;
+
+export default meta;
+
+export const AllScatterCharts: Story = {
+  name: 'All Scatter Charts',
+  args: {
+    filename: '',
+    forcecharttype: "scatter",
+  }
+};
