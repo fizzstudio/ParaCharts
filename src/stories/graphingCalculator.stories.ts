56db62caf8dd79aeb3bb92932781fdfec541502c
Doug Schepers <doug@schepers.cc>
2025-07-10 02:23:19 -0400

    Added graphing calculator story

---
+++ src/stories/graphingCalculator.stories.ts
@@ -0,0 +1,24 @@
+import { Settings } from '../../lib/store/settings_types';
+import { defaults } from '../../lib/store/settings_defaults';
+import { Chart, type ChartProps } from './Chart';
+
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+type Story = StoryObj<ChartProps>;
+
+const meta = {
+  title: "Chart",
+  render: (args) => Chart(args),
+} satisfies Meta<ChartProps>;
+
+export default meta;
+
+export const GraphingCalculator0: Story = {
+  name: "Graphing Calculator",
+  args: {
+    filename: "",
+    forcecharttype: "graph",
+    config: {
+    }
+  }
+}
