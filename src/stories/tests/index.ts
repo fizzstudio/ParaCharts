99e6eae76ce2b9008d6c00dc8931b11acf08483c
Ryan McCleary <remccleary@gmail.com>
2025-08-22 10:31:27 -0400

    Added src/stories/tests directory, defined test function maps

---
+++ src/stories/tests/index.ts
@@ -0,0 +1,26 @@
+import type { ChartType, ChartTypeFamily } from '@fizz/paramanifest';
+
+export const familyTestMap: Record<ChartTypeFamily, any> = {
+    'line': [],
+    'bar': [],
+    'pastry': [],
+    'scatter': [],
+    'histogram': [],
+    'graph': []
+};
+
+export const chartTypeTestMap: Record<ChartType, any> = {
+    'line': [],
+    'stepline': [],
+    'bar': [],
+    'column': [],
+    'lollipop': [],
+    'histogram': [],
+    'scatter': [],
+    'heatmap': [],
+    'pie': [],
+    'donut': [],
+    'graph': []
+};
+
+export const storyTestMap: Record<string, any> = {};
