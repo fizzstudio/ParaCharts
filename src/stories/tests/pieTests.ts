5967a20f3ccd6ebf394c9c0b8d2a066225d9d42e
Ryan McCleary <remccleary@gmail.com>
2025-09-01 20:43:42 -0400

    added test for tab order

--- src/stories/tests/pieTests.ts
+++ src/stories/tests/pieTests.ts
@@ -1,19 +1,35 @@
 import { Test, TestRunner, ExpectFunction } from './TestRunner';
 import * as shadow from 'shadow-dom-testing-library';
+import { waitFor } from 'storybook/test';
 
 export default class PieTestRunner extends TestRunner {
 
     constructor(canvas: any, userEvent: any, expect: ExpectFunction) {
         super(canvas, userEvent, expect);
     }
-    
+
     @Test
-    async exampleTestFn1() {
+    async parachartInDocument() {
         const parachart = await this.canvas.findByTestId('para-chart');
         await this.expect(parachart).toBeInTheDocument();
+    }
+
+    @Test
+    async ariaLabelContainsDatasetTitle() {
+        const parachart = await this.canvas.findByTestId('para-chart');
         const application = shadow.getByShadowRole(parachart, 'application');
         const ariaLabel = application.getAttribute('aria-label');
         await this.expect(ariaLabel).toContain(this.manifest.datasets[0].title);
     }
+
+    @Test
+    async tabOrder() {
+        const parachart = await this.canvas.findByTestId('para-chart');
+        await this.userEvent.tab();
+        await this.userEvent.tab();
+        console.log(document.activeElement?.children[2] + '');
+        this.expect(document.activeElement).toHaveAttribute('role', 'application');
+    }
+
 
 }
