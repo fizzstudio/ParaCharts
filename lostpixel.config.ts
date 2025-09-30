db8e3d1e415b99caeb32cf1a403c409ec400187b
Ryan McCleary <remccleary@gmail.com>
2025-09-30 14:35:19 -0400

    Updated workflow

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,5 +1,6 @@
 import { CustomProjectConfig } from 'lost-pixel';
 
+// Ran in MinGW with `docker run --rm -v '//c/Users/remcc/paracharts:/usr/workdir' -e 'WORKSPACE=/usr/workdir' -e DOCKER=1 -e LOST_PIXEL_MODE=update lostpixel/lost-pixel:v3.22.0`
 export const config: CustomProjectConfig = {
   storybookShots: {
     storybookUrl: './storybook-static',
