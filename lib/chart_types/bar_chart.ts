ce099f3145b16285715e42d0300d673e651f4994
simonvarey <simonvarey@gmail.com>
2025-11-17 21:33:36 -0500

    Fixed series labels for queries

--- lib/chart_types/bar_chart.ts
+++ lib/chart_types/bar_chart.ts
@@ -229,10 +229,12 @@ export class BarChartInfo extends PlaneChartInfo {
         msgArray = this.describeChord(visitedDatapoints);
       } */
       const seriesKey = queriedNode.options.seriesKey;
-      const datapointCount = this._store.model!.atKey(seriesKey)!.length;
+      const series = this._store.model!.atKey(seriesKey)!;
+      const datapointCount = series.length;
+      const seriesLabel = series.getLabel();
       msgArray.push(interpolate(
-        queryMessages.seriesKeyLength,
-        { seriesKey, datapointCount }
+        queryMessages.seriesLabelLength,
+        { seriesLabel, datapointCount }
       ));
     } else if (queriedNode.isNodeType('datapoint')) {
       /*
@@ -249,17 +251,19 @@ export class BarChartInfo extends PlaneChartInfo {
       const selectedDatapoints = this._store.selectedDatapoints;
       const seriesKey = queriedNode.options.seriesKey;
       const index = queriedNode.options.index;
-      const datapoint = this._store.model!.atKey(seriesKey)!.datapoints[index];
+      const series = this._store.model!.atKey(seriesKey)!;
+      const datapoint = series.datapoints[index];
+      const seriesLabel = series.getLabel();
       // XXX yuck
       const datapointView = this._store.paraChart.paraView.documentView!.chartLayers.dataLayer.datapointView(seriesKey, index)!;
 
       msgArray.push(interpolate(
-        queryMessages.datapointKeyLength,
+        queryMessages.datapointLabelLength,
         {
-          seriesKey,
+          seriesLabel,
           datapointXY: formatXYDatapoint(datapoint, 'raw'),
           datapointIndex: queriedNode.options.index + 1,
-          datapointCount: this._store.model!.atKey(seriesKey)!.length
+          datapointCount: series.length
         }
       ));
 
