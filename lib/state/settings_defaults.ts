777435196c30788b012a432172661a8724d56ac7
Josh Fennell <josh@jfnl.org>
2026-02-11 22:06:51 -0500

    Re-enable `barWidth` setting.
    Closes #1029.

--- lib/state/settings_defaults.ts
+++ lib/state/settings_defaults.ts
@@ -207,7 +207,7 @@ export const defaults: Settings = {
   type: {
     bar: {
       stacking: 'standard',
-      barWidth: 20,
+      barWidth: 0,
       colorByDatapoint: false,
       isDrawTotalLabels: true,
       totalLabelGap: 5,
@@ -230,7 +230,7 @@ export const defaults: Settings = {
     },
     column: {
       stacking: 'standard',
-      barWidth: 10,
+      barWidth: 0,
       colorByDatapoint: false,
       isDrawTotalLabels: false,
       totalLabelGap: 10,
