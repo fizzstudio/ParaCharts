be65d9f9356928b66054a302c23ef48885ff6548
Charlotte McCleary <charlotte.may.mccleary@gmail.com>
2026-02-18 22:06:06 -0500

    Adding hidden SVG before ParaChart for discoverability

--- lib/paraview/paraview.ts
+++ lib/paraview/paraview.ts
@@ -935,6 +935,15 @@ export class ParaView extends ParaComponent {
 
   render(): TemplateResult {
     return html`
+    <svg role="img" 
+    aria-label= "Press tab to interact with ParaChart"
+    height="1" 
+    width="1" 
+    tabindex="0" 
+    viewBox="0 0 1 1" 
+    xmlns="http://www.w3.org/2000/svg">
+      <rect x="0" y="0" width="1" height="1" fill="none" stroke="none" />
+    </svg>
     <div ${ref(this._containerRef)} @fullscreenchange=${() => this._onFullscreenChange()}>
     <svg
         role="application"
