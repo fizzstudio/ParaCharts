d1473ca096c1c5c0f86680d7e8be7cf5f0c94746
simonvarey <simonvarey@gmail.com>
2026-03-27 14:55:15 -0400

    Added more charts to short description demo

--- src/demo/short-desc/demo.ts
+++ src/demo/short-desc/demo.ts
@@ -5,14 +5,19 @@ import { initParaSummary, summarizerFromModel } from '@fizz/parasummary';
 import '../../../lib-ai/index-ai';
 
 import SINGLE_LINE_MANIFEST from '../../demo-data/line-single-manifest-7.json';
+import MULTI_LINE_MANIFEST from '../../demo-data/line-multi-manifest-57.json';
+import PIE_MANIFEST from '../../demo-data/pie-manifest-dark-matter.json';
 
 await initParaSummary();
 
-const singleLinePara = document.getElementById('single_line') as HTMLParagraphElement;
+async function displayShortDesc(elementId: string, manifest: unknown): Promise<void> {
+  const paraElement = document.getElementById(elementId) as HTMLParagraphElement;
+  const model = modelFromInlineManifest(manifest as Manifest);
+  const summarizer = summarizerFromModel(model);
+  const shortDesc = await summarizer.getRequestedSummaries(['$.datasets[0]._short']);
+  paraElement.innerText = shortDesc.text;
+}
 
-const singleLineModel = modelFromInlineManifest(SINGLE_LINE_MANIFEST as Manifest);
-const singleLineSummarizer = summarizerFromModel(singleLineModel);
-
-const singleLineShort = await singleLineSummarizer.getRequestedSummaries(['$.datasets[0]._short']);
-
-singleLinePara.innerText = singleLineShort.text;
+displayShortDesc('single_line', SINGLE_LINE_MANIFEST);
+displayShortDesc('multi_line', MULTI_LINE_MANIFEST);
+displayShortDesc('pie', PIE_MANIFEST);
