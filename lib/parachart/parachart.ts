37f435ff313e63e80379234fae3b8db84039b09b
Josh Fennell <josh@jfnl.org>
2026-04-14 19:21:16 -0400

    Add `pollinterval` attr.

--- lib/parachart/parachart.ts
+++ lib/parachart/parachart.ts
@@ -57,6 +57,8 @@ export class ParaChart extends ParaComponent {
   @property({ type: Boolean }) scalable = false;
   @property() accessor manifest = '';
   @property() manifestType: SourceKind = 'url';
+  /** Manifest update polling interval in seconds */
+  @property({ type: Number }) pollInterval = 0;
   // `data` must be a URL, if set
   @property() data = '';
   @property({type: Object}) accessor config: SettingsInput = {};
@@ -150,6 +152,11 @@ export class ParaChart extends ParaComponent {
         if (this.manifest && !this.headless) {
           this.runLoader(this.manifest, this.manifestType).then(() => {
             this.log.info('ParaCharts fully initialized');
+            if (this.pollInterval && this.manifestType === 'url') {
+              setInterval(() => {
+                this.runLoader(this.manifest, this.manifestType);
+              }, this.pollInterval*1000);
+            }
             this._scrollyteller = new Scrollyteller(this);
           });
         } else if (this.getElementsByTagName("table")[0]) {
