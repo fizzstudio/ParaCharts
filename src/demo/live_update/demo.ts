043e85427c3ad9dcfb0ac2519e5b8c5ceef80739
Josh Fennell <josh@jfnl.org>
2026-04-16 17:43:50 -0400

    Add live update demos.

---
+++ src/demo/live_update/demo.ts
@@ -0,0 +1,91 @@
+import '../../../lib-ai/index-ai';
+
+import { ParaChartAi } from '../../../lib-ai/index-ai';
+import { PlaneChartInfo } from '../../../lib/chart_types';
+
+const MANI = '/src/demo/live_update/line-single-manifest-1047.json';
+const MIN = 35;
+const MAX = 65;
+const RANGE = MAX - MIN;
+const AUTO_INTERVAL_MS = 500;
+
+let mani: any;
+let url: string;
+let interval: ReturnType<typeof setInterval> | null =  null;
+
+const addBtn = document.getElementById('add-btn')!;
+const startBtn = document.getElementById('start-btn')!;
+const resetBtn = document.getElementById('reset-btn')!;
+const maniText = document.getElementById('manifest')!;
+
+const chart = document.getElementById('chart') as ParaChartAi;
+
+addBtn.addEventListener('click', () => {
+  if (interval) return;
+  addRecord();
+});
+startBtn.addEventListener('click', () => {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+    return;
+  }
+  interval = setInterval(async () => {
+    await addRecord();
+  }, AUTO_INTERVAL_MS);
+});
+resetBtn.addEventListener('click', () => {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+  }
+  reset();
+});
+
+async function addRecord() {
+  // mani.jim.datasets[0].series[0].records[0].x
+  const data = mani.jim.datasets[0].series[0].records;
+  data.splice(0, 1);
+  const prevY = parseFloat(data.at(-1)!.y);
+  const pctDelta = Math.random()*0.3 - 0.15;
+  // const yInterval = (chart.paraState.chartInfo as PlaneChartInfo).yInterval!;
+  // const yRange = yInterval.end - yInterval.start;
+  data.push({
+    x: nextX(data.at(-1)!.x),
+    y: `${prevY + pctDelta*RANGE}`
+  });
+  await updateChart();
+}
+
+function nextX(x: string) {
+  let [q, y] = x.split(' ');
+  const qnum = (parseInt(q[1]) - 1 + 1) % 4;
+  q = `Q${qnum + 1}`;
+  if (!qnum) {
+      y = `${parseInt(y) + 1}`;
+  }
+  return q + ' ' + y;
+}
+
+async function reset() {
+  mani = await (await fetch(MANI)).json();
+  await updateChart();
+}
+
+async function updateChart() {
+  const maniStr = JSON.stringify(mani, null, 2);
+  maniText.textContent = maniStr;
+  const blob = new Blob([maniStr], {
+    type: 'application/json',
+  });
+  if (url) {
+    URL.revokeObjectURL(url);
+  }
+  url = URL.createObjectURL(blob);
+  chart.api.setManifest(url);
+  await chart.loaded;
+}
+
+await chart.loaded;
+
+await reset();
