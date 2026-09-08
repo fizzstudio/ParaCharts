c105ad982319bc4305f5ba421298e1ccc3153472
simonvarey <simonvarey@gmail.com>
2026-09-08 10:14:41 -0400

    Moved combo stories to correct folder

--- src/stories/autogen/combo45.stories.ts
+++ src/stories/autogen/combo45.stories.ts
@@ -5,7 +5,7 @@ import type { Meta, StoryObj } from '@storybook/web-components-vite';
 type Story = StoryObj<ChartProps>;
 
 const meta = {
-  title: 'Charts/Bar Charts/Single Bar Charts',
+  title: 'Charts/Combo Charts',
   render: (args) => Chart(args),
 } satisfies Meta<ChartProps>;
 
@@ -15,6 +15,6 @@ export const Chart45: Story = {
   name: "Sales (45)",
   args: {
     filename: 'manifests/combo-manifest-ms.json',
-    forcecharttype: 'bar',
+    forcecharttype: 'combo',
   }
 }
